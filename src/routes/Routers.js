0e75fe23ea868aa3def443535f622007618ea865
Sowmiya13G <sowmiyag13.doodleblue@gmail.com>
2024-06-10 18:28:06 +0530

    products page done , func in prog

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -3,6 +3,9 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "../pages/home";
 import Cart from "../pages/cart";
+import Checkout from "../pages/checkOut";
+import Pizzas from "../pages/pizzas";
+import PizzaDetails from "../pages/pizzaDetails";
 
 const Routers = () => {
   return (
@@ -10,6 +13,9 @@ const Routers = () => {
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element={<Home />} />
       <Route path="/cart" element={<Cart />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/pizzas" element={<Pizzas />} />
+      <Route path="/pizzas/:id" element={<PizzaDetails />} />
     </Routes>
   );
 };
